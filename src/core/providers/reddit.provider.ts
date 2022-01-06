b78c8d5c7b99eeb30dd3081921456d19a7bc784d
James Swift <james.rt.swift@gmail.com>
2022-01-06 14:29:13 +0000

    separate RedditProvider and subredditStream

--- src/core/providers/reddit.provider.ts
+++ src/core/providers/reddit.provider.ts
@@ -5,12 +5,7 @@ import SnooStream from '../lib/snoostream.lib'
 import { OrFail } from '../lib/helper.lib'
 import Pollify from '../lib/pollify.lib'
 import EventEmitter from 'events'
-
-export declare interface RedditProvider {
-    on(event: 'comment', listener: (user: Snoowrap.RedditUser, comment: Snoowrap.Comment) => void): this
-    on(event: 'submission', listener: (user: Snoowrap.RedditUser, submission: Snoowrap.Submission) => void): this
-    on(event: 'error', listener: (...data: any[]) => void): this
-}
+import { subredditStream } from './streamable.provider';
 
 export class RedditProvider extends EventEmitter {
 
@@ -28,9 +23,6 @@ export class RedditProvider extends EventEmitter {
             username: OrFail(process.env.REDDIT_USERNAME),
             password: OrFail(process.env.REDDIT_PASSWORD)
         });
-
-        this._streamer = new SnooStream(this._client);
-        this.createListeners();
     }
 
     private static _instance?: RedditProvider;
@@ -49,40 +41,9 @@ export class RedditProvider extends EventEmitter {
     //
     // Manage Polling
     //
-    private _streamer: SnooStream;
-    private _commentStream?: Pollify;
-    private _submissionStream?: Pollify;
 
-    private createListeners(): void {
-        this._commentStream = this._streamer.commentStream(OrFail(process.env.REDDIT_SUBREDDIT), { rate: 10000 });
-        this._commentStream?.on('post', this.onComment.bind(this))
-        this._commentStream?.on('error', this.onError.bind(this))
-
-        this._submissionStream = this._streamer.submissionStream(OrFail(process.env.REDDIT_SUBREDDIT), { rate: 10000 });
-        this._submissionStream?.on('post', this.onSubmission.bind(this))
-        this._submissionStream?.on('error', this.onError.bind(this))
+    public createListeners( subreddit: string ) : subredditStream {
+        return new subredditStream( this, subreddit )
     }
-
-    //
-    //  Event Handling
-    //
-
-    private onComment(comment: Snoowrap.Comment): void {
-        void comment.author.fetch().then(((user: Snoowrap.RedditUser) => {
-            this.emit('comment', user, comment)
-        }).bind(this))
-    }
-
-    private onSubmission(submission: Snoowrap.Submission): void {
-        void submission.author.fetch().then(((user: Snoowrap.RedditUser) => {
-            this.emit('submission', user, submission)
-        }).bind(this))
-    }
-
-    //
-    //  Error Handling
-    //
-    private onError(error: any) {
-        this.emit('error', error)
-    }
+   
 }
---
+++ src/core/providers/streamable.provider.ts
@@ -0,0 +1,61 @@
+import { RedditProvider } from "./reddit.provider";
+import Snoowrap, { Subreddit } from "snoowrap";
+import SnooStream from "../lib/snoostream.lib";
+import Pollify from "../lib/pollify.lib";
+import { EventEmitter } from "stream";
+
+export declare interface subredditStream {
+    on(event: 'comment', listener: (user: Snoowrap.RedditUser, comment: Snoowrap.Comment) => void): this
+    on(event: 'submission', listener: (user: Snoowrap.RedditUser, submission: Snoowrap.Submission) => void): this
+    on(event: 'error', listener: (...data: any[]) => void): this
+}
+
+export class subredditStream extends EventEmitter{
+
+    private _redditProvider: RedditProvider;
+    private _streamer: SnooStream;
+    private _commentStream?: Pollify;
+    private _submissionStream?: Pollify;
+    private _subreddit: string;
+
+    public constructor(redditProvider: RedditProvider, subreddit: string){
+        super()
+        this._redditProvider = redditProvider;
+        this._streamer = new SnooStream(this._redditProvider.getRedditClient());
+        this._subreddit = subreddit
+    }
+
+    private createListeners(): void {
+        this._commentStream = this._streamer.commentStream(this._subreddit, { rate: 10000 });
+        this._commentStream?.on('post', this.onComment.bind(this))
+        this._commentStream?.on('error', this.onError.bind(this))
+
+        this._submissionStream = this._streamer.submissionStream(this._subreddit, { rate: 10000 });
+        this._submissionStream?.on('post', this.onSubmission.bind(this))
+        this._submissionStream?.on('error', this.onError.bind(this))
+    }
+
+    //
+    //  Event Handling
+    //
+
+    private onComment(comment: Snoowrap.Comment): void {
+        void comment.author.fetch().then(((user: Snoowrap.RedditUser) => {
+            this.emit('comment', user, comment)
+        }).bind(this))
+    }
+
+    private onSubmission(submission: Snoowrap.Submission): void {
+        void submission.author.fetch().then(((user: Snoowrap.RedditUser) => {
+            this.emit('submission', user, submission)
+        }).bind(this))
+    }
+
+    //
+    //  Error Handling
+    //
+    private onError(error: any) {
+        this.emit('error', error)
+    }
+
+}
