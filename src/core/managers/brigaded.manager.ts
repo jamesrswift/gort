df63f660a787b42b9d98a53c5d1829bf77e9503a
James Swift <james.rt.swift@gmail.com>
2022-01-07 21:26:31 +0000

    muteManager class created

---
+++ src/core/managers/brigaded.manager.ts
@@ -0,0 +1,9 @@
+import { logging } from '../logging';
+
+const logger = logging.getLogger('core.manager.brigadeManager');
+
+export default class brigadeManager {
+    private static _instance?: brigadeManager;
+    public static get Instance(): brigadeManager { return this._instance || (this._instance = new brigadeManager()); }
+    private constructor() { }
+}
---
+++ src/core/managers/watched.manager.ts
@@ -0,0 +1,9 @@
+import { logging } from '../logging';
+
+const logger = logging.getLogger('core.manager.watchedManager');
+
+export default class watchedManager {
+    private static _instance?: watchedManager;
+    public static get Instance(): watchedManager { return this._instance || (this._instance = new watchedManager()); }
+    private constructor() { }
+}
