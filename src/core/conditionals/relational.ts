128d901caa9e0a8981e3b36bea5dd57a28c3f831
James Swift <james.rt.swift@gmail.com>
2022-01-10 16:57:43 +0000

    Improve relational conditionals and restrict types

--- src/core/conditionals/relational.ts
+++ src/core/conditionals/relational.ts
@@ -4,28 +4,43 @@ import Snoowrap from "snoowrap";
 //----------------------------------------------------------------------------------
 //  Relational arithmetic operations
 //----------------------------------------------------------------------------------
-
-function relationalOperator<tLHS, tRHS, tReturn>(predicate: (lhs: tLHS, rhs: tRHS) => tReturn) {
-    return class extends executable<tReturn> {
-        private _predicate: (lhs: tLHS, rhs: tRHS) => tReturn = predicate;
-        private _lhs: executable<tLHS>;
-        private _rhs: executable<tRHS>;
-        constructor(lhs: executable<tLHS>, rhs: executable<tRHS>) {
-            super();
-            this._lhs = lhs;
-            this._rhs = rhs;
-        }
-        public override execute(args: executableArguments): Promise<tReturn> {
-            return new Promise<tReturn>(async (resolve, reject) => {
-                resolve(this._predicate(await this._lhs.execute(args), await this._rhs.execute(args)))
-            })
-        }
+abstract class relationalOperator<tLHS, tRHS, tReturn> extends executable<tReturn>{
+    private _lhs: executable<tLHS>;
+    private _rhs: executable<tRHS>;
+    constructor(lhs: executable<tLHS>, rhs: executable<tRHS>) {
+        super();
+        this._lhs = lhs;
+        this._rhs = rhs;
     }
+    public override execute(args: executableArguments): Promise<tReturn> {
+        return new Promise<tReturn>(async (resolve, reject) => {
+            resolve(this._predicate(await this._lhs.execute(args), await this._rhs.execute(args)))
+        })
+    }
+    protected abstract _predicate( lhs: tLHS, rhs: tRHS) : tReturn ;
+
 }
 
-export class equals extends relationalOperator((lhs: any, rhs: any): boolean => { return lhs == rhs }) { };
-export class notequals extends relationalOperator((lhs: any, rhs: any): boolean => { return lhs != rhs }) { };
-export class greaterThan extends relationalOperator((lhs: any, rhs: any): boolean => { return lhs > rhs }) { };
-export class lessThan extends relationalOperator((lhs: any, rhs: any): boolean => { return lhs < rhs }) { };
-export class greaterThanOrEquals extends relationalOperator((lhs: any, rhs: any): boolean => { return lhs >= rhs }) { };
-export class lessThanOrEquals extends relationalOperator((lhs: any, rhs: any): boolean => { return lhs <= rhs }) { };
+export class equals<Type> extends relationalOperator<Type, Type, boolean> {
+    protected override _predicate( lhs: Type, rhs: Type) : boolean { return lhs == rhs; }
+}
+
+export class notequals<Type> extends relationalOperator<Type, Type, boolean> {
+    protected override _predicate( lhs: Type, rhs: Type) : boolean { return lhs != rhs; }
+}
+
+export class greaterThan<Type> extends relationalOperator<Type, Type, boolean> {
+    protected override _predicate( lhs: Type, rhs: Type) : boolean { return lhs > rhs; }
+}
+
+export class lessThan<Type> extends relationalOperator<Type, Type, boolean> {
+    protected override _predicate( lhs: Type, rhs: Type) : boolean { return lhs < rhs; }
+}
+
+export class greaterThanOrEquals<Type> extends relationalOperator<Type, Type, boolean> {
+    protected override _predicate( lhs: Type, rhs: Type) : boolean { return lhs >= rhs; }
+}
+
+export class lessThanOrEquals<Type> extends relationalOperator<Type, Type, boolean> {
+    protected override _predicate( lhs: Type, rhs: Type) : boolean { return lhs <= rhs; }
+}
