cece70c86fe9ef395cd22a503cab0063904eea3f
James Swift <james.rt.swift@gmail.com>
2022-01-10 18:57:49 +0000

    begin work on database integration

---
+++ src/core/providers/database.provider.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose"
+import dotenv from 'dotenv';
+import { OrFail } from "../lib/helper.lib";
+import { logging } from '../logging';
+
+const logger = logging.getLogger('core.provider.databaseProvider');
+
+dotenv.config();
+let _mongoURI = "mongodb://" + OrFail(process.env.DATABASE_URI) + "?authSource=admin";
+mongoose.connect( _mongoURI )
+    .then( ()=>{logger.debug(`MongoDB connected ${_mongoURI}`)})
+    .catch(logger.error);
+
+export default mongoose
