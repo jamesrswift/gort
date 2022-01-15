125cdc8df4c74daada251d24fb4e5114871bd09a
James Swift <james.rt.swift@gmail.com>
2022-01-15 22:16:59 +0000

    brigadeOrigin now correctly gets thread ID

--- src/core/conditionals/brigaded.ts
+++ src/core/conditionals/brigaded.ts
@@ -6,8 +6,7 @@ export class brigadeOrigin extends executable<string> {
 	public override async execute(args: executableArguments): Promise<string> {
 		// Get threadID
 		if (args.targetType == 'Submission') return ''; // New threads can't be brigaded
-		const threadID: string = (<Comment>args.target).parent_id; // I hope parent_id is the id of the thread
-
+		const threadID: string = (<Comment>args.target).link_id.slice(3);
 		// Check if thread is on brigaded list
 		if (await brigadeManager.Instance.isTargetOnBrigadeList(threadID)) {
 			const info = await brigadeManager.Instance.getBrigadeEntryInfo(
