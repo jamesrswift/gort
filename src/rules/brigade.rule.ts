ad4f4c37046547cfd4fcab415bea9a8feb50ccb7
James Swift <james.rt.swift@gmail.com>
2022-01-15 22:17:15 +0000

    Updated brigade rule text to reflect event

--- src/rules/brigade.rule.ts
+++ src/rules/brigade.rule.ts
@@ -12,7 +12,7 @@ export class brigadeRule extends ruleBase {
 
 	Action = new notify({
 		color: '#db3838',
-		message: 'User contributed to "bad" subreddits',
+		message: 'This user is potentially brigading! They have a history in a subreddit that has linked to the thread they are now commenting on!',
 		channelID: '931921060826349578',
 	});
 }
