7cb81e8588633f31cbd14a3e1afcee6a94086068
James Swift <james.rt.swift@gmail.com>
2022-01-15 14:55:12 +0000

    fix help command new lines

--- src/commands/help.command.ts
+++ src/commands/help.command.ts
@@ -11,12 +11,12 @@ class helpCommand extends commandBase {
 		cmd: string,
 		discordMessage: Discord.Message
 	): Promise<string | undefined | null> {
-		let output: string = 'List of commands: \n';
+		let output: string = 'List of commands: \r\n';
 
 		const commands = commandHandler.Instance.getCommands();
 		const commandHandle = commandHandler.Instance.getHandle();
 		for (let [commandName, command] of commands) {
-			output += `\t${command.name}, \t${command.description}, \t${commandHandle} ${command.usage}`;
+			output += `\t${command.name}, \t${command.description}, \t${commandHandle} ${command.usage}\r\n`;
 		}
 
 		return output;
