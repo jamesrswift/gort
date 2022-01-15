7b5c621a55951dedd2f2bac94323c5416bab763c
James Swift <james.rt.swift@gmail.com>
2022-01-15 14:57:37 +0000

    Enable logging output to console

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-import gort from "./bot/gort";
+import { logging, LogEntry } from './core/logging';
 
+logging
+	.configure({
+		minLevels: {
+			'': 'trace',
+		},
+	})
+	.registerConsoleLogger();
+
+import gort from './bot/gort';
 const bot = new gort();
