9d3076d1bf5f01157281899802ec963521346e70
James Swift <james.rt.swift@gmail.com>
2022-01-17 20:33:29 +0000

    Remove verbose arguments

--- src/commands/help.command.ts
+++ src/commands/help.command.ts
@@ -1,4 +1,3 @@
-import Discord from 'discord.js';
 import commandBase, { commandHandler } from '../core/command.class';
 
 class helpCommand extends commandBase {
@@ -6,11 +5,7 @@ class helpCommand extends commandBase {
 	description = 'show a list of available commands';
 	usage = 'help';
 
-	override async execute(
-		args: string[],
-		cmd: string,
-		discordMessage: Discord.Message
-	): Promise<string | undefined | null> {
+	override async execute(): Promise<string | undefined | null> {
 		let output: string = 'List of commands: \r\n';
 
 		const commands = commandHandler.Instance.getCommands();
