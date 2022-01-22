db95510336264901386d071dfab41152a2749819
James Swift <james.rt.swift@gmail.com>
2022-01-22 15:24:06 +0000

    Look at all attributes

--- src/core/conditionals/toxicity.ts
+++ src/core/conditionals/toxicity.ts
@@ -21,6 +21,31 @@ export class toxitityTrigger extends executable<boolean> {
 		this._options = options;
 	}
 
+	public scoresDecision(value: IAttributeScores){
+		if ( value['TOXICITY'] > 0.95 ) return true
+		if ( value['SEVERE_TOXICITY'] > 0.95 ) return true
+		if ( value['IDENTITY_ATTACK'] > 0.95 ) return true
+		if ( value['INSULT'] > 0.95 ) return true
+		if ( value['PROFANITY'] > 0.95 ) return true
+		if ( value['THREAT'] > 0.95 ) return true
+
+		// Experimental values:
+		if ( value['SEXUALLY_EXPLICIT'] > 0.95 ) return true
+		if ( value['FLIRTATION'] > 0.95 ) return true
+
+		// New York Times attributes
+		if ( value['ATTACK_ON_AUTHOR'] > 0.95 ) return true
+		if ( value['ATTACK_ON_COMMENTER'] > 0.95 ) return true
+		if ( value['INCOHERENT'] > 0.95 ) return true
+		if ( value['INFLAMMATORY'] > 0.95 ) return true
+		if ( value['LIKELY_TO_REJECT'] > 0.95 ) return true
+		if ( value['OBSCENE'] > 0.95 ) return true
+		if ( value['SPAM'] > 0.95 ) return true
+		//if ( value['UNSUBSTANTIAL'] > 0.95 ) return true
+
+		return false
+	}
+
 	public override async execute(args: executableArguments): Promise<boolean> {
 		return new Promise<boolean>((resolve, reject) => {
 			const html =
@@ -32,7 +57,7 @@ export class toxitityTrigger extends executable<boolean> {
 			this._client
 				.getScores(html, this._options)
 				.then((value: IAttributeScores) => {
-					return value['SPAM'] > 0.95 || value['TOXICITY'] > 0.95;
+					resolve(this.scoresDecision(value))
 				});
 		});
 	}
