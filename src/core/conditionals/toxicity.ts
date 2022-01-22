97f36314f9fb63457e7c087bf18047af501033fc
James Swift <james.rt.swift@gmail.com>
2022-01-22 15:27:21 +0000

    Include documentation of toxicity attributes

--- src/core/conditionals/toxicity.ts
+++ src/core/conditionals/toxicity.ts
@@ -22,26 +22,26 @@ export class toxitityTrigger extends executable<boolean> {
 	}
 
 	public scoresDecision(value: IAttributeScores){
-		if ( value['TOXICITY'] > 0.95 ) return true
-		if ( value['SEVERE_TOXICITY'] > 0.95 ) return true
-		if ( value['IDENTITY_ATTACK'] > 0.95 ) return true
-		if ( value['INSULT'] > 0.95 ) return true
-		if ( value['PROFANITY'] > 0.95 ) return true
-		if ( value['THREAT'] > 0.95 ) return true
+		if ( value['TOXICITY'] > 0.95 ) return true // A rude, disrespectful, or unreasonable comment that is likely to make people leave a discussion.
+		if ( value['SEVERE_TOXICITY'] > 0.95 ) return true // A very hateful, aggressive, disrespectful comment or otherwise very likely to make a user leave a discussion or give up on sharing their perspective. This attribute is much less sensitive to more mild forms of toxicity, such as comments that include positive uses of curse words.
+		if ( value['IDENTITY_ATTACK'] > 0.95 ) return true // Negative or hateful comments targeting someone because of their identity.
+		if ( value['INSULT'] > 0.95 ) return true // Insulting, inflammatory, or negative comment towards a person or a group of people.
+		if ( value['PROFANITY'] > 0.95 ) return true // Swear words, curse words, or other obscene or profane language.
+		if ( value['THREAT'] > 0.95 ) return true // Describes an intention to inflict pain, injury, or violence against an individual or group.
 
 		// Experimental values:
-		if ( value['SEXUALLY_EXPLICIT'] > 0.95 ) return true
-		if ( value['FLIRTATION'] > 0.95 ) return true
+		if ( value['SEXUALLY_EXPLICIT'] > 0.95 ) return true // Contains references to sexual acts, body parts, or other lewd content.
+		if ( value['FLIRTATION'] > 0.95 ) return true // Pickup lines, complimenting appearance, subtle sexual innuendos, etc.
 
 		// New York Times attributes
-		if ( value['ATTACK_ON_AUTHOR'] > 0.95 ) return true
-		if ( value['ATTACK_ON_COMMENTER'] > 0.95 ) return true
-		if ( value['INCOHERENT'] > 0.95 ) return true
-		if ( value['INFLAMMATORY'] > 0.95 ) return true
-		if ( value['LIKELY_TO_REJECT'] > 0.95 ) return true
-		if ( value['OBSCENE'] > 0.95 ) return true
-		if ( value['SPAM'] > 0.95 ) return true
-		//if ( value['UNSUBSTANTIAL'] > 0.95 ) return true
+		if ( value['ATTACK_ON_AUTHOR'] > 0.95 ) return true // Attack on the author of an article or post. 
+		if ( value['ATTACK_ON_COMMENTER'] > 0.95 ) return true // Attack on fellow commenter.
+		if ( value['INCOHERENT'] > 0.95 ) return true // Difficult to understand, nonsensical.
+		if ( value['INFLAMMATORY'] > 0.95 ) return true // Intending to provoke or inflame.
+		if ( value['LIKELY_TO_REJECT'] > 0.95 ) return true // Overall measure of the likelihood for the comment to be rejected according to the NYT's moderation.
+		if ( value['OBSCENE'] > 0.95 ) return true // Obscene or vulgar language such as cursing.
+		if ( value['SPAM'] > 0.95 ) return true // Irrelevant and unsolicited commercial content.
+		//if ( value['UNSUBSTANTIAL'] > 0.95 ) return true // Trivial or short comments
 
 		return false
 	}
